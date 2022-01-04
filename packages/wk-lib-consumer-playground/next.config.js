ba5bcc9b80c9f5ee0a85694365ed5e61f07cd5b3
Alex Eagleson <alexeagleson@gmail.com>
2022-01-04 11:42:47 -0500

    [UPD] align changes with wk-lerna-lib-playground name change

--- packages/wk-lib-consumer-playground/next.config.js
+++ packages/wk-lib-consumer-playground/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require('next-transpile-modules')(['@weknow-network/wk-lib-playground']); // pass the modules you would like to see transpiled
+const withTM = require('next-transpile-modules')(['@weknow-network/wk-lerna-lib-playground']); // pass the modules you would like to see transpiled
 
 module.exports = withTM({
   reactStrictMode: true,
