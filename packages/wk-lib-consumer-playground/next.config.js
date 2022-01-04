4c88cf6dcf9c0db64540784e6ad4bede72bd1f8d
Alex Eagleson <alexeagleson@gmail.com>
2022-01-03 23:26:34 -0500

    [UPD] sync typescript versions and add next-transpile-modules

--- packages/wk-lib-consumer-playground/next.config.js
+++ packages/wk-lib-consumer-playground/next.config.js
@@ -1,3 +1,6 @@
-module.exports = {
+const withTM = require('next-transpile-modules')(['@weknow-network/wk-lib-playground']); // pass the modules you would like to see transpiled
+
+module.exports = withTM({
   reactStrictMode: true,
-}
+});
+
